5a80e9a4fd4064e2d8fc3782020eb53c84a8d942
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-30 02:44:39 -0300

    Criando Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import { Flex, Icon, Input, Text } from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export function Header() {
+  return (
+    <Flex
+      w="100%"
+      as="header"
+      maxW={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      align="center"
+      px="6"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        dashgo
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxW={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Buscar na plataforma"
+          _placeholder={{
+            color: 'gray.400',
+          }}
+        />
+
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+    </Flex>
+  );
+}
