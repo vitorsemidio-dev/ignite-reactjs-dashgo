6ee144504601f08f5b896b32e8771c024c3cabcb
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-30 03:02:44 -0300

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,15 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <div>
+    <Flex direction="column" h="100vh">
       <Header />
-      <h1>Dashboard</h1>
-    </div>
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
