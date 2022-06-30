473355300dacc5c7c40de2263c205bf73a82dc82
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-30 04:22:34 -0300

    Componente: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function userList() {
@@ -80,6 +81,8 @@ export default function userList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
