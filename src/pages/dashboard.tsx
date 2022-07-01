daf1bc8a33cf77240cf29f6cafde2886793d4387
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-01 01:44:39 -0300

    Dashboard Responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -77,13 +77,13 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
