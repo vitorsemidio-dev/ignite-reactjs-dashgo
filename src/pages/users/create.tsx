6b730423b69243c7579c9b889fe2835571d8ae5d
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-01 01:58:05 -0300

    Cadastro Responsivo

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -19,20 +19,20 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={['6', '8']}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth={'240px'} spacing="8" w="100%">
+          <VStack spacing={['6', '8']}>
+            <SimpleGrid minChildWidth={'240px'} spacing={['6', '8']} w="100%">
               <Input name="name" label="Nome Completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth={'240px'} spacing="8" w="100%">
+            <SimpleGrid minChildWidth={'240px'} spacing={['6', '8']} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_confirmation"
@@ -42,7 +42,7 @@ export default function CreateUser() {
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
+          <Flex mt={['6', '8']} justify="flex-end">
             <HStack spacing="4">
               <Button colorScheme="whiteAlpha">Cancelar</Button>
               <Button colorScheme="pink">Salvar</Button>
