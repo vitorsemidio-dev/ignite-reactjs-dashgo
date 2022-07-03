d9adcef6f3b9a27e85e6e6089926ed629fa54b18
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-03 00:30:59 -0300

    Sinalizando refetch dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,7 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, error, isFetching, isLoading } = useQuery(
     'users',
     async () => {
       const response = await fetch('http://localhost:3000/api/users');
@@ -64,6 +64,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
